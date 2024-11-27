c52f18152d0429593e1ae1fbac3e1124eaba6816
ghson <ghson@ocube.co.kr>
2024-11-27 16:32:17 +0900

    대충 수정

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
 const holiday = new Holiday();
-holiday.getHoliday();
+holiday
+  .getHoliday()
+  .then((response) => response.json())
+  .then((data) => {
+    const holidayArray = Array.from(data);
+    holidayArray.forEach((day) => {
+      const date = day.date;
+      const name = day.localName;
+
+      const div = document.createElement("div");
+      div.innerHTML = `${date} -> ${name}`;
+
+      document.body.appendChild(div);
+    });
+  });
